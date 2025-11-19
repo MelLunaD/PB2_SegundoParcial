0c30d4a93d6665893806711f4e8e73098462d6ad
Eros Panuccio <erospanuccio@gmail.com>
2025-11-18 22:52:29 -0300

    Agrego clase GestionDeCriaturas y su test

---
+++ test/ar/edu/unlam/pb2/gestiondecriaturas/GestionDeCriaturasTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam.pb2.gestiondecriaturas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GestionDeCriaturasTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
